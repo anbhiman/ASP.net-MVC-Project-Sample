9eae869ddc05afd6c07c0d0e5e28092b33f9254d
anbhiman <40796080+anbhiman@users.noreply.github.com>
2018-07-04 18:16:16 +0530

    mvc demo

---
+++ MVCAzureDemo/MVCAzureDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAzureDemo.Startup))]
+namespace MVCAzureDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
